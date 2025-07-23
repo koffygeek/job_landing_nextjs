e2e87db454cc5d283a71cd7b799a622225005a00
Yvan Grebe <yvan.grebe@gmail.com>
2025-07-23 18:08:18 +0100

    installation prerequisitie

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const font = Roboto({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Job portal | Landing Page",
+  description: "Job portal landing page using with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
