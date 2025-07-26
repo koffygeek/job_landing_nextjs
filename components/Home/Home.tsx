e6fdaed9a8da86d374eaa33a34757b6537b40cc0
Yvan Grebe <yvan.grebe@gmail.com>
2025-07-26 17:57:50 +0100

    Hero section

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Hero from './Hero/Hero'
 
 const Home = () => {
     return (
-        <div className='overflow-hidden h-[2000px]:'></div>
+        <div className='overflow-hidden h-[2000px]:'>
+            <Hero />
+            <Category />
+        </div>
     )
 }
 
